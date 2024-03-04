58c1f03383adf654a736d7149a959a2363a7e7a7
Mahant Lacximikant <223073@student.wqe.ac.uk>
2024-03-04 14:13:30 +0000

    button 1

--- Car Racing Game/level1.Designer.cs
+++ Car Racing Game/level1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // gameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 486);
+            this.ClientSize = new System.Drawing.Size(942, 503);
+            this.Controls.Add(this.button1);
             this.Name = "gameForm";
             this.Text = "Game";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameForm_KeyDown);
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
