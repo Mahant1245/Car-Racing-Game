0b15fedca9ba7cb3770e2c0e88174179ad53a190
Mahant1245 <mahant1245@gmail.com>
2024-03-07 09:31:09 +0000

    working

--- Car Racing Game/globalClass.cs
+++ Car Racing Game/globalClass.cs
@@ -5,14 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Car_Racing_Game;//this is global class
-
-internal static class globalClass
-{
-    public static int sec;
-    public static class time
+namespace Car_Racing_Game//worked noww
+{//this is global class
+    internal static class globalClass
     {
-        public static DateTime beginTime { get; set; }
-        public static DateTime gameTime { get; set; }
+        public static int sec;
+        public static class time
+        {
+            public static DateTime beginTime { get; set; }
+            public static DateTime gameTime { get; set; }
+        }
     }
 }
