1c1e2de1498336b11f24f3fc3d234054964cb042
Mahant1245 <mahant1245@gmail.com>
2024-03-17 17:50:00 +0000

    player movement done

--- Car Racing Game/level1.Designer.cs
+++ Car Racing Game/level1.Designer.cs
@@ -35,15 +35,16 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
             // 
             // timer
@@ -66,7 +67,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Car_Racing_Game.Properties.Resources.heart;
-            this.pictureBox2.Location = new System.Drawing.Point(519, 7);
+            this.pictureBox2.Location = new System.Drawing.Point(424, 7);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(27, 22);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -93,59 +94,67 @@
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Car_Racing_Game.Properties.Resources.roadMain;
-            this.pictureBox1.Location = new System.Drawing.Point(37, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(476, 509);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(488, 0);
+            this.pictureBox5.Location = new System.Drawing.Point(473, -2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(41, 509);
+            this.pictureBox5.Size = new System.Drawing.Size(44, 510);
             this.pictureBox5.TabIndex = 7;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(21, 0);
+            this.pictureBox6.Location = new System.Drawing.Point(-7, -12);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(41, 509);
+            this.pictureBox6.Size = new System.Drawing.Size(39, 509);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Car_Racing_Game.Properties.Resources.userCar;
+            this.player.Location = new System.Drawing.Point(207, 279);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 121);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 10;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // gameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Car_Racing_Game.Properties.Resources.grass;
-            this.ClientSize = new System.Drawing.Size(551, 510);
+            this.BackgroundImage = global::Car_Racing_Game.Properties.Resources.roadMain;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.player);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.timeLeftLabel);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox1);
             this.MaximizeBox = false;
             this.Name = "gameForm";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.gameForm_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameForm_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,8 +166,9 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Car Racing Game/level1.cs
+++ Car Racing Game/level1.cs
@@ -18,11 +18,40 @@ namespace Car_Racing_Game
             InitializeComponent();
         }
 
+        bool right;
+        bool left;
+        bool speedup;
         private void gameForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Escape)
+            if (e.KeyCode == Keys.A)
             {
+                left = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                right = true;
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                speedup = true;
+            }
+        }
 
+        public void playerMovement()
+        {
+            if(right==true)
+            {
+                if(player.Left<430)
+                {
+                    player.Left += 5;
+                }
+            }
+            if (left==true)
+            {
+                if(player.Left>0)
+                {
+                    player.Left-=5;
+                }
             }
         }
 
@@ -30,11 +59,33 @@ namespace Car_Racing_Game
         {
             timer.Start();
            timeLeftLabel.BringToFront();//brings the label in front
+           
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             timeLeftLabel.Text = globalClass.sec--.ToString();//change the text every sec
+        }
+
+        private void gameForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                speedup = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            playerMovement();
         }
     }
 
